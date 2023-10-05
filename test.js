fb5c654c18e3a2c8539cef8daeb3013a52fef0a0
Omar <omar.nasr@qeema.net>
2023-10-05 10:23:23 +0300

    modified:   test.js

--- test.js
+++ test.js
@@ -1,4 +1,22 @@
-//testing the branches and merging requests
+//testing triggering a function based on condition
 
-console.log('testing');
-console.log("again testing staging");
+let num1 = 10;
+let num2 = 20;
+let total = num1 + num2;
+let trigger1 = () => {
+	document.write("testing the total function");
+	alert("this line to test the first function");
+	console.log(`total is ${num1}+${num2} = ${total}`);
+	// return `total is ${num1}+${num2} = ${total}`;
+};
+let function2 = () => {
+	console.log("testing second function");
+};
+
+let str1 = "add";
+
+if (str1 == "add") {
+	trigger1();
+} else {
+	function2();
+}
